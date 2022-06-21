64411fa8f13c06ac3b6832b71654ebd8e5786950
Sumandari <sumandari.ee@gmail.com>
2022-06-21 13:46:59 +0200

    websocket #1

--- taxi/routing.py
+++ taxi/routing.py
@@ -1,8 +1,17 @@
 from email.mime import application
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from django.urls import path
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from taxi.middleware import TokenAuthMiddlewareStack
+from trips.consumers import TaxiConsumer
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
+    'websocket': TokenAuthMiddlewareStack(
+        URLRouter([
+            path('taxi/', TaxiConsumer.as_asgi())
+        ]),
+    )
 })
